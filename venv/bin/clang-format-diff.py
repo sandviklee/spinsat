39304e46f2e1809d2d5646758889853e4ae6e2b7
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-02-12 09:33:51 +0100

    chore: Remove venv

--- venv/bin/clang-format-diff.py
+++
@@ -1,8 +0,0 @@
-#!/Users/sandviklee/Desktop/Repositories/sandviklee/spinsat/venv/bin/python3.11
-# -*- coding: utf-8 -*-
-import re
-import sys
-from clang_format.clang_format_diff import main
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
