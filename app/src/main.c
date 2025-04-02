f51a040fe55f868ca401099d799310f2a018186d
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-02 13:09:21 +0200

    refactor: Remove custom from main

--- app/src/main.c
+++ app/src/main.c
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "app/lib/custom.h"
+#include <app/lib/bluetooth.h>
 #include <app_version.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -12,8 +12,6 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void) {
   LOG_INF("Hello World! %s\n", APP_VERSION_STRING);
-  int value = custom_get_value(0);
-  LOG_INF("Value: %d\n", value);
 
   return 0;
 }
