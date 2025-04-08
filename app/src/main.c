bec36dc7db26ad3908717f1f3a79924218fe6db3
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-09 00:03:37 +0200

    feat: Update main to test bluetooth

--- app/src/main.c
+++ app/src/main.c
@@ -10,8 +10,20 @@
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
+void state_machine_write(uint8_t state, uint8_t *data, uint16_t len) {
+  LOG_INF("State machine write: %d, len: %d", state, len);
+}
+
 int main(void) {
   LOG_INF("Hello World! %s\n", APP_VERSION_STRING);
+
+  callbacks_t *callbacks = callback_init(state_machine_write);
+
+  int8_t err = bluetooth_init(callbacks);
+  if (err) {
+    LOG_ERR("Bluetooth init failed: %d", err);
+    return err;
+  }
 
   return 0;
 }
