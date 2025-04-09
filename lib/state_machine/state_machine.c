af984bd8003bb336cad7147e6be679b8941bedfe
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-09 14:13:41 +0200

    refactor: Update signature of state machine & Add docs

--- lib/state_machine/state_machine.c
+++ lib/state_machine/state_machine.c
@@ -6,13 +6,21 @@
 #include "app/lib/bluetooth.h"
 #include <app/lib/state_machine.h>
 #include <stdint.h>
+#include <string.h>
 #include <time.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-// #include <zephyr/sys/poweroff.h>
+#include <zephyr/sys/poweroff.h>
 
 LOG_MODULE_REGISTER(state_machine_handle, CONFIG_APP_LOG_LEVEL);
 
+/**
+ * @brief Context for the state machine.
+ *
+ * This structure holds the context data for the state machine, including a
+ * pointer to the data and a timestamp.
+ */
 state_machine_ctx_t *_state_machine_ctx_init(void *data) {
   state_machine_ctx_t *p_ctx =
       (state_machine_ctx_t *)k_malloc(sizeof(state_machine_ctx_t));
@@ -23,25 +31,46 @@ state_machine_ctx_t *_state_machine_ctx_init(void *data) {
       .data = data,
       .time_stamp = k_uptime_get() / 1000,
   };
-  p_ctx = &ctx;
+  *p_ctx = ctx;
 
   return p_ctx;
 }
 
+/**
+ * @brief Destroy the context of the state machine.
+ *
+ * This function frees the memory allocated for the state machine context.
+ *
+ * @param sm Pointer to the state machine instance.
+ */
 void _state_machine_ctx_destroy(state_machine_t *sm) {
   if (sm->ctx != NULL) {
     k_free(sm->ctx);
   }
 }
 
-static state_machine_t *sm;
+static state_machine_t sm_i;
+static state_machine_t *sm = NULL;
 
-uint8_t state_machine_init(state_t s) {
-  sm->last_state = CONFIG_STATE_MACHINE_START_STATE;
-  sm->current_state = s;
+/**
+ * @brief Initialize the state machine.
+ *
+ * This function initializes the state machine by setting the initial state and
+ * event.
+ *
+ * @return 0 on success, -1 on error.
+ */
+uint8_t state_machine_init() {
+  if (sm != NULL) {
+    LOG_ERR("State machine already initialized\n");
+    return -1;
+  }
+  sm = &sm_i;
+  memset(sm, 0, sizeof(state_machine_t));
+  sm->last_state = STATE_OFF;
+  sm->current_state = STATE_INIT;
   sm->event = EVENT_OK;
   sm->ctx = _state_machine_ctx_init(NULL);
-
   if (sm->ctx == NULL) {
     return -1;
   }
@@ -49,16 +78,42 @@ uint8_t state_machine_init(state_t s) {
   return 0;
 }
 
-static void state_machine_write(int state, uint8_t *data, uint16_t len) {
-  LOG_INF("State machine write: %d\n", state);
+/**
+ * @brief Write function for the state machine.
+ *
+ * This function is called to write the current state and data to the state
+ * machine.
+ *
+ * @param state The current state of the state machine.
+ * @param data The data associated with the current state.
+ */
+static void state_machine_write(uint8_t state, uint8_t data) {
+  LOG_INF("STATE: %02x, DATA: %02x\n", state, data);
 
   if (sm == NULL) {
     LOG_ERR("State machine instance is NULL\n");
     return;
   }
+
+  sm->last_state = sm->current_state;
+  sm->current_state = (state_t)state;
+  sm->ctx->data = (void *)&data;
 }
 
+#define LED1_NODE DT_ALIAS(led0)
+#define LED2_NODE DT_ALIAS(led1)
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+
+/**
+ * @brief State machine handle function.
+ *
+ * This function handles the state machine transitions and actions.
+ *
+ * @return 0 on success, -1 on error.
+ */
 uint8_t state_machine_handle() {
+  int ret = 0;
   if (sm == NULL) {
     LOG_ERR("State machine is NULL\n");
     return -1;
@@ -70,20 +125,51 @@ uint8_t state_machine_handle() {
 
   switch (sm->current_state) {
   case STATE_OFF:
+    LOG_INF("SHUTTING DOWN...\n");
+    gpio_pin_set_dt(&led1, 0);
+    gpio_pin_set_dt(&led2, 0);
     _state_machine_shutdown();
+
     break;
   case STATE_INIT:
     LOG_INF("State: INIT\n");
 
     sm->last_state = sm->current_state;
-    sm->current_state = STATE_IDLE;
+    sm->current_state = STATE_CONNECTING;
 
     callbacks_t *callbacks = callback_init(&state_machine_write);
     int err = bluetooth_init(callbacks);
     if (err) {
-      LOG_ERR("Bluetooth init failed (err %d)\n", err);
+      LOG_ERR("Bluetooth init failed1 (err %d)\n", err);
       return -1;
     }
+
+    if (!gpio_is_ready_dt(&led1) || !gpio_is_ready_dt(&led2)) {
+      LOG_ERR("GPIO is not ready\n");
+      return -1;
+    }
+
+    ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+      LOG_ERR("Failed to configure LED1 pin (err %d)\n", ret);
+      return -1;
+    }
+
+    ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+      LOG_ERR("Failed to configure LED2 pin (err %d)\n", ret);
+      return -1;
+    }
+
+    gpio_pin_set_dt(&led1, 1);
+    gpio_pin_set_dt(&led2, 0);
+
+    break;
+  case STATE_CONNECTING:
+    LOG_INF("State: CONNECTING\n");
+
+    gpio_pin_toggle_dt(&led1);
+    gpio_pin_toggle_dt(&led2);
 
     break;
   case STATE_IDLE:
@@ -92,22 +178,30 @@ uint8_t state_machine_handle() {
       // motor_reset();
     }
     LOG_INF("State: IDLE\n");
+    gpio_pin_toggle_dt(&led1);
+    gpio_pin_set_dt(&led2, 0);
     break;
   case STATE_HOLD:
     if (sm->last_state == STATE_SPIN_LEFT ||
         sm->last_state == STATE_SPIN_RIGHT) {
-      // motor_reset();
+        // TODO: Implement the motor reset function...
     }
     // TODO: Implement the PID Controller...
     LOG_INF("State: HOLD\n");
+    gpio_pin_set_dt(&led1, 0);
+    gpio_pin_set_dt(&led2, 1);
     break;
   case STATE_SPIN_LEFT:
     LOG_INF("State: SPIN_LEFT\n");
-    // motor_spin(sm->ctx->data, LEFT);
+    // TODO : Implement the motor spin function
+    gpio_pin_set_dt(&led1, 1);
+    gpio_pin_set_dt(&led2, 0);
     break;
   case STATE_SPIN_RIGHT:
     LOG_INF("State: SPIN_RIGHT\n");
-    // motor_spin(sm->ctx->data, RIGHT);
+    // TODO : Implement the motor spin function
+    gpio_pin_set_dt(&led1, 1);
+    gpio_pin_set_dt(&led2, 1);
     break;
   }
 
@@ -117,6 +211,6 @@ uint8_t state_machine_handle() {
 void _state_machine_shutdown(void) {
   // TODO: Implement the shutdown procedure
   _state_machine_ctx_destroy(sm);
-  // sys_poweroff();
-
+  bluetooth_deinit();
+  sys_poweroff();
 }
