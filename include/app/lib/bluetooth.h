f98cd7f43236e62a23fb143a015263cbc9ace086
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-09 14:12:51 +0200

    refactor: Update UUID & refactor bluetooth conn signatures & state machine write

--- include/app/lib/bluetooth.h
+++ include/app/lib/bluetooth.h
@@ -17,15 +17,13 @@
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
-#define BT_UUID_SPINSAT_VAL BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123)
+#define BT_UUID_SPINSAT_VAL BT_UUID_128_ENCODE(0x00001545, 0x1212, 0xefde, 0x1523, 0x785feabcd123)
 
 #define BT_UUID_SPINSAT_STATE_VAL                                              \
-  BT_UUID_128_ENCODE(0x00001524, 0x1212, 0xefde, 0x1523, 0x785afeabcd124)
+  BT_UUID_128_ENCODE(0x00001546, 0x1212, 0xefde, 0x1523, 0x785afeabcd124)
 
 #define BT_UUID_SPINSAT BT_UUID_DECLARE_128(BT_UUID_SPINSAT_VAL)
 #define BT_UUID_SPINSAT_STATE BT_UUID_DECLARE_128(BT_UUID_SPINSAT_STATE_VAL)
-
-
 
 typedef struct {
   void (*connected)(struct bt_conn *conn, uint8_t err);
@@ -33,10 +31,11 @@ typedef struct {
 } bluetooth_connection_t;
 
 typedef struct {
-  void (*state_machine_write)(int state, uint8_t *data, uint16_t len);
+  void (*state_machine_write)(uint8_t state, uint8_t data);
 } callbacks_t;
 
-int16_t bluetooth_init(callbacks_t *callbacks);
+int8_t bluetooth_init(callbacks_t *callbacks);
+int8_t bluetooth_deinit(void);
 callbacks_t *callback_init(void *state_machine_write);
 
 #ifdef __cplusplus
