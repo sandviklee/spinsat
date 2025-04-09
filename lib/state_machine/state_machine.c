6cb58916b69065dceafd3b87374b0c84fe8990e5
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-09 02:13:46 +0200

    feat: Update state machine to use static memory for initialization

--- lib/state_machine/state_machine.c
+++ lib/state_machine/state_machine.c
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "zephyr/sys/printk.h"
+#include "app/lib/bluetooth.h"
 #include <app/lib/state_machine.h>
 #include <stdint.h>
 #include <time.h>
@@ -28,15 +28,15 @@ state_machine_ctx *_state_machine_ctx_init(void *data) {
   return p_ctx;
 }
 
-uint8_t state_machine_ctx_destroy(state_machine *sm) {
+void _state_machine_ctx_destroy(state_machine *sm) {
   if (sm->ctx != NULL) {
     k_free(sm->ctx);
   }
-
-  return 0;
 }
 
-uint8_t state_machine_init(state_machine *sm, state_t s) {
+static state_machine *sm;
+
+uint8_t state_machine_init(state_t s) {
   sm->last_state = CONFIG_STATE_MACHINE_START_STATE;
   sm->current_state = s;
   sm->event = EVENT_OK;
@@ -49,7 +49,21 @@ uint8_t state_machine_init(state_machine *sm, state_t s) {
   return 0;
 }
 
-uint8_t state_machine_handle(state_machine *sm) {
+static void state_machine_write(int state, uint8_t *data, uint16_t len) {
+  LOG_INF("State machine write: %d\n", state);
+
+  if (sm == NULL) {
+    LOG_ERR("State machine instance is NULL\n");
+    return;
+  }
+}
+
+uint8_t state_machine_handle() {
+  if (sm == NULL) {
+    LOG_ERR("State machine is NULL\n");
+    return -1;
+  }
+
   if (sm->event != EVENT_OK) {
     return sm->event;
   }
@@ -63,29 +77,37 @@ uint8_t state_machine_handle(state_machine *sm) {
 
     sm->last_state = sm->current_state;
     sm->current_state = STATE_IDLE;
+
+    callbacks_t *callbacks = callback_init(&state_machine_write);
+    int err = bluetooth_init(callbacks);
+    if (err) {
+      LOG_ERR("Bluetooth init failed (err %d)\n", err);
+      return -1;
+    }
+
     break;
   case STATE_IDLE:
     if (sm->last_state == STATE_SPIN_LEFT ||
         sm->last_state == STATE_SPIN_RIGHT) {
-      motor_reset();
+      // motor_reset();
     }
     LOG_INF("State: IDLE\n");
     break;
   case STATE_HOLD:
     if (sm->last_state == STATE_SPIN_LEFT ||
         sm->last_state == STATE_SPIN_RIGHT) {
-      motor_reset();
+      // motor_reset();
     }
     // TODO: Implement the PID Controller...
     LOG_INF("State: HOLD\n");
     break;
   case STATE_SPIN_LEFT:
     LOG_INF("State: SPIN_LEFT\n");
-    motor_spin(sm->ctx->data, LEFT);
+    // motor_spin(sm->ctx->data, LEFT);
     break;
   case STATE_SPIN_RIGHT:
     LOG_INF("State: SPIN_RIGHT\n");
-    motor_spin(sm->ctx->data, RIGHT);
+    // motor_spin(sm->ctx->data, RIGHT);
     break;
   }
 
@@ -95,5 +117,7 @@ uint8_t state_machine_handle(state_machine *sm) {
 void _power_off(void) {
   // TODO: Impl. checks for safe power _power_off
   // - Check if all storage procedures have been finished
+  _state_machine_ctx_destroy(sm);
   // sys_poweroff();
+
 }
